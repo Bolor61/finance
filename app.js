bc20c97f3c1af12f48c4239da62f7aa65bb1ef0b
Bolor61 <63471365+Bolor61@users.noreply.github.com>
2020-06-11 21:38:14 -0500

    tusviig delegtsend update hiine

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var uiController = (function () {
     addBtn: ".add__btn",
     incomeList: ".income__list",
     expenseList: ".expenses__list",
+    tusuvLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expenseLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage",
   };
 
   return {
@@ -42,6 +46,21 @@ var uiController = (function () {
       // for (var i = 0; i < fieldsArr.length; i++) {
       //   fieldsArr[i].value = "";
       // }
+    },
+
+    tusviigUzuuleh: function (tusuv) {
+      document.querySelector(DOMstrings.tusuvLabel).textContent = tusuv.tusuv;
+      document.querySelector(DOMstrings.incomeLabel).textContent =
+        tusuv.totalInc;
+      document.querySelector(DOMstrings.expenseLabel).textContent =
+        tusuv.totalExp;
+      if (tusuv.huvi !== 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi + "%";
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi;
+      }
     },
 
     addListItem: function (item, type) {
@@ -185,8 +204,8 @@ var appController = (function (uiController, financeController) {
       // 5. Эцсийн үлдэгдэл,
       var tusuv = financeController.tusviigAvan();
 
-      //Төсвийн тооцоог дэлэгцэнд гаргана.
-      console.log(tusuv);
+      //6. Төсвийн тооцоог дэлэгцэнд гаргана.
+      uiController.tusviigUzuuleh(tusuv);
     }
   };
 
@@ -206,8 +225,15 @@ var appController = (function (uiController, financeController) {
 
   // public servece
   return {
+    //  programm ehlenguut duuddag function
     init: function () {
       console.log(" Application started ... ");
+      uiController.tusviigUzuuleh({
+        tusuv: 0,
+        huvi: 0,
+        totalInc: 0,
+        totalExp: 0,
+      });
       setupEventlisteners();
     },
   };
